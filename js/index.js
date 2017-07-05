b0f7f567932b8da00ce497a2121a2c6fe37c184e
Vivek Kaushal <vivek.kaushal@outlook.com>
2017-07-06 01:25:46 +0530

    Star Wars

--- js/index.js
+++ js/index.js
@@ -1,98 +1,82 @@
-'use strict';
+/*
+ * Star Wars opening crawl from 1977
+ * 
+ * I freaking love Star Wars, but could not find
+ * a web version of the original opening crawl from 1977.
+ * So I created this one.
+ *
+ * I wrote an article where I explain how this works:
+ * http://timpietrusky.com/star-wars-opening-crawl-from-1977
+ * 
+ * Watch the Start Wars opening crawl on YouTube.
+ * https://www.youtube.com/watch?v=7jK-jZo6xjY
+ * 
+ * Stuff I used:
+ * - CSS (animation, transform)
+ * - HTML audio (the opening theme)
+ * - SVG (the Star Wars logo from wikimedia.org)
+ *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
+ * - JavaScript (to sync the animation/audio)
+ *
+ * Thanks to Craig Buckler for his amazing article 
+ * which helped me to create this remake of the Star Wars opening crawl. 
+ * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
+ *
+ * Sound copyright by The Walt Disney Company.
+ * 
+ *
+ * 2013 by Tim Pietrusky
+ * timpietrusky.com
+ * 
+ */
+StarWars = (function() {
+  
+  /* 
+   * Constructor
+   */
+  function StarWars(args) {
+    // Context wrapper
+    this.el = $(args.el);
+    
+    // Audio to play the opening crawl
+    this.audio = this.el.find('audio').get(0);
+    
+    // Start the animation
+    this.start = this.el.find('.start');
+    
+    // The animation wrapper
+    this.animation = this.el.find('.animation');
+    
+    // Remove animation and shows the start screen
+    this.reset();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var max_particles = 1000;
-
-var tela = document.createElement('canvas');
-tela.width = $(window).width();
-tela.height = $(window).height();
-$("body").append(tela);
-
-var canvas = tela.getContext('2d');
-
-var Particle = function () {
-  function Particle(canvas, progress) {
-    _classCallCheck(this, Particle);
-
-    var random = Math.random();
-    this.progress = 0;
-    this.canvas = canvas;
-
-    this.x = $(window).width() / 2 + (Math.random() * 200 - Math.random() * 200);
-    this.y = $(window).height() / 2 + (Math.random() * 200 - Math.random() * 200);
-    this.s = Math.random() * 1;
-    this.a = 0;
-    this.w = $(window).width();
-    this.h = $(window).height();
-    this.radius = random > .2 ? Math.random() * 1 : Math.random() * 3;
-    this.color = random > .2 ? "#2E4765" : "#b5ff00";
-    this.radius = random > .8 ? Math.random() * 2 : this.radius;
-    this.color = random > .8 ? "#2E4765" : this.color;
-
-    // this.color  = random > .1 ? "#ffae00" : "#f0ff00" // Alien
-    this.variantx1 = Math.random() * 300;
-    this.variantx2 = Math.random() * 400;
-    this.varianty1 = Math.random() * 100;
-    this.varianty2 = Math.random() * 120;
+    // Start the animation on click
+    this.start.bind('click', $.proxy(function() {
+      this.start.hide();
+      this.audio.play();
+      this.el.append(this.animation);
+    }, this));
+    
+    // Reset the animation and shows the start screen
+    $(this.audio).bind('ended', $.proxy(function() {
+      this.audio.currentTime = 0;
+      this.reset();
+    }, this));
   }
-
-  Particle.prototype.render = function render() {
-    this.canvas.beginPath();
-    this.canvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    this.canvas.lineWidth = 2;
-    this.canvas.fillStyle = this.color;
-    this.canvas.fill();
-    this.canvas.closePath();
+  
+  /*
+   * Resets the animation and shows the start screen.
+   */
+  StarWars.prototype.reset = function() {
+    this.start.show();
+    this.cloned = this.animation.clone(true);
+    this.animation.remove();
+    this.animation = this.cloned;
   };
 
-  Particle.prototype.move = function move() {
-    this.x += Math.cos(this.a) * this.s;
-    this.y += Math.sin(this.a) * this.s;
-    this.a += Math.random() * 0.8 - 0.4;
+  return StarWars;
+})();
 
-    if (this.x < 0 || this.x > this.w - this.radius) {
-      return false;
-    }
-
-    if (this.y < 0 || this.y > this.h - this.radius) {
-      return false;
-    }
-    this.render();
-    this.progress++;
-    return true;
-  };
-
-  return Particle;
-}();
-
-var particles = [];
-var init_num = popolate(max_particles);
-function popolate(num) {
-  for (var i = 0; i < num; i++) {
-    setTimeout(function () {
-      particles.push(new Particle(canvas, i));
-    }.bind(this), i * 20);
-  }
-  return particles.length;
-}
-
-function clear() {
-  canvas.globalAlpha = 0.05;
-  canvas.fillStyle = '#000155';
-  canvas.fillStyle = '#000021'; // Alien
-  canvas.fillRect(0, 0, tela.width, tela.height);
-  canvas.globalAlpha = 1;
-}
-
-function update() {
-  particles = particles.filter(function (p) {
-    return p.move();
-  });
-  if (particles.length < init_num) {
-    popolate(1);
-  }
-  clear();
-  requestAnimationFrame(update.bind(this));
-}
-update();
+new StarWars({
+  el : '.starwars'
+});
